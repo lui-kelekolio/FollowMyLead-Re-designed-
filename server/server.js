911490629a0f4784323913109c859d65c76c8de7
Student <student@eda-bc-06.localdomain>
2020-02-27 11:39:36 +1300

    added getWalker data

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const express = require('express')
 
 const server = express()
 
+const walkerRoute = require('./routes/walkerRoute')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
+server.use('/api/walker', walkerRoute)
 module.exports = server
