a87223bb6f5012b502e4ab8a5ad2b8df4a6cc4f7
lui-kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-02-27 18:56:54 +1300

    added css to display dog photos in circles

--- server/server.js
+++ server/server.js
@@ -5,22 +5,16 @@ const server = express()
 
 // Lui and Izzy code
 const walkerRoute = require('./routes/walkerRoute')
-<<<<<<< HEAD
 const dogRoute = require('./routes/dogRoute')
-=======
 const authRoute = require('./routes/authRoute')
->>>>>>> dev
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
-<<<<<<< HEAD
 server.use('/api/walker', walkerRoute)
 // Lui and Izzy code
 server.use('/api/dogs', dogRoute)
-=======
-server.use('/api/v1/walker', walkerRoute)
+// server.use('/api/v1/walker', walkerRoute)
 // server.use('/api/Owner', ownerRoute)
 server.use('/api/v1', authRoute)
->>>>>>> dev
 module.exports = server
