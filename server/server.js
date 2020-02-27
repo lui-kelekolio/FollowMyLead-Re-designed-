91b9bb095befc64e72ca882764b6d111d954f2eb
lui-kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-02-28 11:04:05 +1300

    stage shit

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ server.use(express.static(path.join(__dirname, './public')))
 server.use('/api/walker', walkerRoute)
 // Lui and Izzy code
 server.use('/api/dogs', dogRoute)
-// server.use('/api/v1/walker', walkerRoute)
-// server.use('/api/Owner', ownerRoute)
+server.use('/api/v1/walker', walkerRoute)
+server.use('/api/Owner', ownerRoute)
 server.use('/api/v1', authRoute)
 module.exports = server
