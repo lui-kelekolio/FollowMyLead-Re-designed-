9902bda606265c61ee85e6ac42804ed7dfd9ae82
Scott Thomson <scottthomson_57@hotmail.com>
2020-02-27 15:40:36 +1300

    adding owner server details

--- server/server.js
+++ server/server.js
@@ -4,9 +4,15 @@ const express = require('express')
 const server = express()
 
 const walkerRoute = require('./routes/walkerRoute')
+const ownerRoute = require('./routes/ownerRoute')
+
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
-
+server.use()
 server.use('/api/walker', walkerRoute)
+server.use('/api/owner/:id', ownerRoute)
+
+
+
 module.exports = server
