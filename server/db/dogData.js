0df0c0e43aa5272e864b4ab5c76f92ba8981624c
izzy-asplet <isabelasplet@gmail.com>
2020-02-28 16:00:56 +1300

    dog list rendering with names under each dog

--- server/db/dogData.js
+++ server/db/dogData.js
@@ -2,17 +2,17 @@ const { generateHash } = require('authenticare/server')
 
 const connection = require('./connection')
 
-function getDogs (db = connection) {
+function getDogs(db = connection) {
     return db('dog_table').select()
 }
 
 function getDog(id, db = connection) {
     return db('dog_table')
-    .where('id', id)
-    .first()
+        .where('id', id)
+        .first()
 }
 
 module.exports = {
     getDogs,
-    getDog 
-    }
+    getDog
+}
