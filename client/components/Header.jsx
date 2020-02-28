54a423d1a2cf8d4b551642c3ef4867dfc099c505
Student <student@eda-bc-06.localdomain>
2020-02-29 10:09:43 +1300

    push before mailto:start

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,12 +1,21 @@
 import React from "react"
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { logOff } from 'authenticare/client'
+
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 
 function Header() {
-    return(
+
+    return (
         <div className="header">
             <h1>Follow My Lead</h1>
-            <button><Link className="button-basic" to = '/' >Home</Link></button>
-            <button><Link className="button-basic" to = '/Login' >Login</Link></button>
+            <button><Link className="button-basic" to='/' >Home</Link></button>
+            <IfNotAuthenticated>
+                <button><Link className="button-basic" to='/Login' >Login</Link></button>
+            </IfNotAuthenticated>
+            <IfAuthenticated>
+                <button><Link to='#' onClick={logOff}>Logout</Link></button>
+            </IfAuthenticated>
         </div>
     )
 }
