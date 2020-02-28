a317198cb843be8cfd30b813acb1ea539900fa6c
Scott Thomson <scottthomson_57@hotmail.com>
2020-02-28 15:33:59 +1300

    update owner function

--- server/db/ownerData.js
+++ server/db/ownerData.js
@@ -3,34 +3,41 @@ const config = require('../../knexfile')
 const env = process.env.NODE_ENV || 'development'
 const connection = knex(config[env])
 
-function getOwners(db =connection) {
+function getOwners(db = connection) {
     return db('owner_table').select()
 }
 
 
 function getOwner(id, db = connection) {
     return db('owner_table')
-    .where('id', id)
-    .first()
+        .where('id', id)
+        .first()
 }
 
-function addOwner(first_name, last_name, photo, location, email, db=connection){
+function addOwner(first_name, last_name, photo, location, email, db = connection) {
     return db('owner_table')
-    .insert({
-        first_name: first_name,
-        last_name: last_name,
-        photo: photo,
-        location: location,
-        email: email
-    })
-    .then(id => {
-        return id[0]
-    })
+        .insert({
+            first_name: first_name,
+            last_name: last_name,
+            photo: photo,
+            location: location,
+            email: email
+        })
+        .then(id => {
+            return id[0]
+        })
+}
+
+function updateOwner(id, owner, db = connection) {
+    return db('owner_table')
+        .where('id', id)
+        .update(owner)
 }
 
 
 module.exports = {
     getOwners,
     getOwner,
-    addOwner
+    addOwner,
+    updateOwner
 }
