958898e8f5580ea623adb11bd1428a9e53f514e2
izzy-asplet <isabelasplet@gmail.com>
2020-03-03 08:50:26 +1300

    dogseeds

--- client/components/DogProfile.jsx
+++ client/components/DogProfile.jsx
@@ -135,17 +135,18 @@ class DogProfile extends React.Component {
                 <button><Link to='/doglist'>Dog list</Link></button>
                 <br />
                 <button><Link to={'/walker/' + this.state.walker_id}>Profile</Link></button>
-                <h2>Name: {this.state.name}</h2>
                 <img className='dogprofilephoto' src={this.state.photo} />
+                <h2>Name: {this.state.name}</h2>
+                <h2>Suburb: {this.state.suburb}</h2>
                 <h2>Breed: {this.state.breed}</h2>
                 <h2>Sex: {this.state.sex}</h2>
                 <h2>Size: {this.state.size}</h2>
                 <h2>Walk Length: {this.state.activity_requirements}</h2>
                 <h2>Good with other dogs: {this.state.good_with_other_dogs}</h2>
                 <h2>Special Requirements: {this.state.special_requirements}</h2>
-                <h2>Vet Name: {this.state.vet_name}</h2>
+                <h2>Vet Practice: {this.state.vet_name}</h2>
                 <h2>Vet Contact: {this.state.vet_contact}</h2>
-                <h2>Suburb: {this.state.suburb}</h2>
+                
             </div>
         )
     }
