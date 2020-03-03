809aacb008f1110e499af4b080ea9818f7e2ea63
izzy-asplet <isabelasplet@gmail.com>
2020-03-04 11:02:43 +1300

    move done

--- client/components/DogProfile.jsx
+++ client/components/DogProfile.jsx
@@ -130,55 +130,11 @@ class DogProfile extends React.Component {
         console.log('id deets' + this.state.walker_id)
         return (
             <>
-<<<<<<< HEAD
                 <h1 className="page-title">{this.state.name}'s profile</h1>
                 <div className="profile-container">
-||||||| merged common ancestors
-            <h1 className="page-title">{this.state.name}'s profile</h1>
-            <div className="profile-container">
-                
-                <img className="profile-picture" src={this.state.photo} />
-                <p>Suburb: {this.state.suburb}</p>
-                <br />
-
-                <div className="profile-info">
-
-
-                    <p>Name:{this.state.name}</p>
-                    <p>Breed: {this.state.breed}</p>
-                    <p>Sex: {this.state.sex}</p>
-                    <p>Size: {this.state.size}</p>
-                    <p>Walk Length: {this.state.activity_requirements}</p>
-                    <p>Good with other dogs: {this.state.good_with_other_dogs}</p>
-                    <p>Special Requirements: {this.state.special_requirements}</p>
-                    <p>Vet Practice: {this.state.vet_name}</p>
-                    <p>Vet Contact: {this.state.vet_contact}</p>
-                    
-=======
-            <h1 className="page-title">{this.state.name}'s profile</h1>
-            <div className="profile-container">
-                
-                <img className="profile-picture" src={this.state.photo} />
-                <p> <strong>Suburb: </strong> {this.state.suburb}</p>
-                <br />
-
-                <div className="profile-info">
-
-
-                    <p>Name:{this.state.name}</p>
-                    <p>Breed: {this.state.breed}</p>
-                    <p>Sex: {this.state.sex}</p>
-                    <p>Size: {this.state.size}</p>
-                    <p>Walk Length: {this.state.activity_requirements}</p>
-                    <p>Good with other dogs: {this.state.good_with_other_dogs}</p>
-                    <p>Special Requirements: {this.state.special_requirements}</p>
-                    <p>Vet Practice: {this.state.vet_name}</p>
-                    <p>Vet Contact: {this.state.vet_contact}</p>
-                    
->>>>>>> eee08c2d71a11334435eb71a0b0293af592d1a3e
 
                     <img className="profile-picture" src={this.state.photo} />
-                    <p>Suburb: {this.state.suburb}</p>
+                    <p> <strong>Suburb: </strong> {this.state.suburb}</p>
                     <br />
 
                     <div className="profile-info">
@@ -194,12 +150,11 @@ class DogProfile extends React.Component {
                         <p>Vet Practice: {this.state.vet_name}</p>
                         <p>Vet Contact: {this.state.vet_contact}</p>
 
-
                     </div>
                     {this.state.walker_id !== 0 ? <button className="button" name='sendButton' onClick={this.handleClick}>Send request to the dog's owner</button> : null}
 
                     {this.state.walk_the_dog && <p>You would like to walk this dog. Click the request button to contact the owner</p>}
-                    {this.state.request_sent && <div><p>Great, your request has been sent to this dog's owner. They should be in touch soon!</p><br/>
+                    {this.state.request_sent && <div><p>Great, your request has been sent to this dog's owner. They should be in touch soon!</p><br />
                         <p>Check out</p><a className='button' className='move' href="http://www.google.com">MOVE</a><p>for some awesome walking spots in Wellington</p></div>
                     }
 
