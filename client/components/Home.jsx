025b82cbfb8556de40e0bb57b93e4e2adfd8f3b4
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-09-06 16:24:47 +1200

    Update Home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import Footer from './Footer'
 import About from './About'
 import WhoAreWe from './WhoAreWe'
+import HowItWorks from "./HowItWorks"
 
 
 
@@ -19,10 +20,10 @@ class Home extends React.Component {
             <>
                 <main>
 
-                    <div className="hero-image">
-                        <div className="hero-text"></div>
-                        <img className="home-image" src="/images/ice-dog.jpg"
-                            alt="hero-dog-image" />
+               
+                    <img className="home-image" src="/images/ice-dog.jpg"
+                            alt="hero-dog-image"/>
+                        
                         <div className="hero-page-headline">
                             <div className="home-page-headline">Follow My Lead!</div>
                             <div className="home-page-ptag">Partnering People and Pooches!</div>
@@ -30,12 +31,15 @@ class Home extends React.Component {
                             <div className="register-buttons">
                             <Link className="button" to='/Register/Owner' >Register to have your dog walked</Link>
                             <Link className="button" to='/Register/Walker' >Register to walk a dog</Link>
-                        </div>
+                            </div>
 
                         </div>
 
-                    </div>
+                 
+
+
                 <WhoAreWe />
+                <HowItWorks />
                 <About />
 
                 
