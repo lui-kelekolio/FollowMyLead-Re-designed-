c63b21ce2496e5bdb217a09aa8c7244a41a46ad0
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-09-08 18:48:03 +1200

    Update AboutUs.jsx

--- client/components/AboutUs.jsx
+++ client/components/AboutUs.jsx
@@ -11,14 +11,21 @@ function AboutUs() {
 
             <div className="blurb-container">
                 <div className="blurb-text">
-                <p>(From Left to Right)</p>
-                <h2>Rich, Scott, Izzy, Seb, Lui, Josh.</h2>
+                    <h3>Richard, Scott, Izzy, Sebastian, Lui, Joshua</h3>
+                    <p>(From left to right)</p>
+                </div>
 
+                <div className="body-content">
+                    <p>We are a group of graduate web developers from En Spiral Dev Academy. Follow My Lead is a website application we created as our final group project. This idea was pitched to our class and from there the six of us decided that this idea was definitely worth creating for our final project. 
+                        We spent ten consecutive twelve hour days working on this project, getting it ready to present to family, friends and potential employers. 
+                        <br></br><br></br>
+                        This website is not the original - I have decided to redesign the website itself to further improve my front-end skills. You may find a link to the original github repository here : </p>
+                        <a className="fml-link" href="https://github.com/kahu-2020/Follow-My-Lead" >Follow My Lead</a>
+
+        
                 </div>
             </div>
         </div>
-      <br />
-      <br />
 
     </div>
   );
