b412df1afebc1bbf5491e6ecb32fc279abee5def
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-09-08 17:32:14 +1200

    Update About.jsx

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,27 +1,46 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
 
 function About() {
   return (
     <div className="about">
       <h1 className="page-title">About us! </h1>
+
       <div className="group-photo-container">
       <img className="group-photo1" src="images/Group_photo.jpg" />
-      {/* <img className="group-photo" src="images/Group-photo-2.jpg" /> */}
-      </div>
-      <div className="blurb-container">
-        <div className="blurb-text">
-          <p>Rich, Izzy, Josh, Seb, Lui, Scott.</p>
-          <p>We are a group of web development students at EDA Wellington.  We love dogs and coding, and decided to combine these interests by
-          building 'Follow My Lead', a platform where dog-less dog lovers and
-          busy dog owners can meet to arrange dog walks!</p>
-          
+      <div className="overlay1">
+        <div className="link-to-about-us">
+        <Link to='/About-Us' >More About Us</Link>
         </div>
       </div>
-      <br />
-      <br />
-      <div>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <div className="blurb-container">
+          <div className="blurb-text">
+    
+            <p>We are a group of web development students at EDA Wellington.  We love dogs and coding, and decided to combine these interests by
+            building 'Follow My Lead', a platform where dog-less dog lovers and
+            busy dog owners can meet to arrange dog walks!</p>
+    
+            
+          </div>
+        </div>
       </div>
+
+      <br />
+      <br />
     </div>
   );
 }
