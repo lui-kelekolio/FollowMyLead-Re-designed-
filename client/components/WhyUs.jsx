35b202825698739d12edcc527f930234478f2b69
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-09-08 19:51:47 +1200

    Create WhyUs.jsx

---
+++ client/components/WhyUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+function WhyUs() {
+  return (
+    <div className="why-us">
+      <h1 className="page-title">Why pick us?</h1>
+    </div>
+  );
+}
+
+export default WhyUs;
